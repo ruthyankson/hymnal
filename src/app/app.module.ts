7de4808cf27bc274c5db010d21749114eff1492c
adwowaray <ruthyankson357@gmail.com>
2024-05-12 22:37:41 +0000

    App components, services, models, constants, folders, and modules set up for development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SongPageComponent } from './song-page/song-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SongPageComponent
   ],
   imports: [
     BrowserModule,
