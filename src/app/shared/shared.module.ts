b9c765b7415c4e153a6137e7efe5c7278be30279
adwowaray <ruthyankson357@gmail.com>
2024-05-12 23:19:07 +0000

    Added shared module to app module and imported and exported the header and search components in the shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    SearchComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
-  ]
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    HeaderComponent,
+    SearchComponent
+  ],
+  providers:[]
 })
 export class SharedModule { }
