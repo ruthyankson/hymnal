c673626f79cc899ce52886b0c631c7e385b6a200
adwowaray <ruthyankson357@gmail.com>
2024-06-06 02:13:31 +0000

    Search box init

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HymnModel } from '../../models/hymn.model';
+import { HYMNS } from '../../constants/hymns';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+  hymns: HymnModel[] = HYMNS;
 
+  // heroes$!: Observable<Hero[]>;
+  // private searchTerms = new Subject<string>();
+
+  // constructor(private heroService: HeroService) {}
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    // this.searchTerms.next(term);
+  }
+
+  // ngOnInit(): void {
+  //   this.heroes$ = this.searchTerms.pipe(
+  //     // wait 300ms after each keystroke before considering the term
+  //     debounceTime(300),
+
+  //     // ignore new term if same as previous term
+  //     distinctUntilChanged(),
+
+  //     // switch to new search observable each time the term changes
+  //     switchMap((term: string) => this.heroService.searchHeroes(term)),
+  //   );
+  // }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { SearchComponent } from './components/search/search.component';
 import { SongComponent } from './components/song/song.component';
 import { ModalComponent } from './components/modal/modal.component';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -15,11 +16,13 @@ import { ModalComponent } from './components/modal/modal.component';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterLink
   ],
   exports: [
     CommonModule,
     FormsModule,
+    RouterLink,
     HeaderComponent,
     SearchComponent,
     SongComponent
